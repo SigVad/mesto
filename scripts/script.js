26d518df603f3f369de7502c2f2a584413ac5897
SigVad <sigachev.va@yandex.ru>
2022-06-04 16:35:01 +0300

    feature/ карточки добавляются через template

--- scripts/script.js
+++ scripts/script.js
@@ -10,9 +10,76 @@ const profileElement = document.querySelector('.profile');
 const editButton = profileElement.querySelector('.profile__edit-button');
 const profileName = profileElement.querySelector('.profile__name');
 const profileProfession = profileElement.querySelector('.profile__profession');
+
+const addCardButton = profileElement.querySelector('.profile__add-button');
 //код клавиши Esc, Enter для наглядности
 //const ESC_KEY_CODE = 27;
 //const ENTER_KEY_CODE = 13;
+
+// извлекаем список карточек для заполнения по шаблону
+const cardsList = document.querySelector('.elements__list');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  },
+]; 
+
+function addCardInMassive(massive, newCardLink, newCardName) {
+  elem={
+    name: '',
+    link: ''
+  };
+  elem.name = newCardName;
+  elem.link = newCardLink;
+  massive.push(elem);
+  console.log(massive);
+}
+
+let newCardName = 'Домбай';
+let newCardLink = './images/element/kirill-pershin-1556355-unsplash.jpg';
+// addCardInMassive(initialCards, newCardLink, newCardName);
+
+//функция добавить карту на страницу по шаблону
+function cardInDOM(cardLink, cardName) {
+  const cardTemplate = document.querySelector('#cards-template').content;
+
+  console.log(cardTemplate);
+  const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+  
+  console.log(cardElement);
+  cardElement.querySelector('.element__image').src = cardLink;
+  cardElement.querySelector('.element__title').textContent = cardName;
+  cardsList.append(cardElement);
+}
+
+for (let i=0; i < initialCards.length; i++){
+  let cardLink = initialCards[i].link;
+  let cardName = initialCards[i].name;
+  cardInDOM(cardLink, cardName);
+}
+
 
 //функции открыть и закрыть попап
 function openPopup() {
@@ -23,9 +90,8 @@ function openPopup() {
   profileProfessionInput.value = profileProfession.textContent;
   //обработчик закрытия на Esc
   //document.addEventListener('keyup', onDocumentKeyUp)
-  
 }
-// кнопка сохранения не работает
+// кнопка сохранения
 function savePopup(evt) {
   evt.preventDefault();
   profileName.textContent =  profileNameInput.value;
@@ -42,6 +108,7 @@ function closePopup() {
 //   if (event.keyCode === ENTER_KEY_CODE){savePopup()}}
 
 //слушатели события нажатий на кнопки
+//addCardButton.addEventListener('click', cardInDOM(newCardLink, newCardName));
 editButton.addEventListener('click', openPopup);
 formInput.addEventListener('submit', savePopup);
 closeButton.addEventListener('click', closePopup);
