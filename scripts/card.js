cf67883779a9a6d6799cbbfb58bf3b9e342d36ef
SigVad <sigachev.va@yandex.ru>
2022-07-28 14:15:26 +0300

    fix/убрать отрисовку карт из класса

--- scripts/card.js
+++ scripts/card.js
@@ -1,26 +1,28 @@
 
-import {openPopup, cardsList, popupImage, imageSrc, imageCaption, cardsTemplate} from './index.js';
+import openPopup from './index.js';
+
+const cardsTemplate = document.querySelector('#cards-template');
+const popupImage = document.querySelector('.popup_type_image');
+const imageSrc = popupImage.querySelector('.popup__image');
+const imageCaption = popupImage.querySelector('.popup__image-caption');
 
 export default class Card {
   constructor(link, name, templateSelector) {
     this._tSelector = templateSelector;
-    this.link = link;
-    this.name = name;
+    this._link = link;
+    this._name = name;
   }
-  // // публичная функция отрисовки карты (ссылка, название)
-  // renderCard(){
-  //   cardsList.prepend(this._createCard());
-  // }
   // приватный метод делает разметку
   // забираем разметку из HTML и клонируем элемент
   _getTemplate() {
     // найдёт template-элемент с классом card-template, извлечёт его содержимое,
     // в содержимом найдёт элемент с классом card, клонирует его
-    const cardElement = cardsTemplate.content.querySelector(this._tSelector).cloneNode(true);
+    const cardElement = cardsTemplate.content
+                        .querySelector(this._tSelector).cloneNode(true);
     // вернём DOM-элемент карточки
     return cardElement;
   }
-  //функция добавления карты на страницу по шаблону (ссылка, название)
+  //публичный метод добавления карты на страницу по шаблону (ссылка, название)
   createCard() {//
     // Запишем разметку в приватное поле _element. 
     // Так у других элементов появится доступ к ней.
@@ -35,22 +37,18 @@ export default class Card {
     // Вернём элемент наружу
     return this._element;
   }
-
+  //метод добавления слушателей
   _setEventListeners() {
-    //слушатель лайка
     this._element.querySelector('.element__like-button').addEventListener('click', () => {
       this._handleLikeClick();
     });
-    //слушатель удаления
     this._element.querySelector('.element__trash-button').addEventListener('click', () => {
       this._handleTrashClick();
     });
-    //слушатель просмотра
     this._element.querySelector('.element__image').addEventListener('click', () => {
       this._handleOpenPopupImage();
     });
   }
-
   _handleLikeClick() {
     this._element.querySelector('.element__like-button').classList.toggle('element__like-button_active');
   }
