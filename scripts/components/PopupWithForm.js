3ea765bd98d7ced8ec333d380f6c7ffe179efe65
Vadim Sigachev <sigachev.va@yandex.ru>
2022-08-21 19:37:29 +0300

    fix/догрузить файл попап формы

---
+++ scripts/components/PopupWithForm.js
@@ -0,0 +1,35 @@
+import Popup from "./Popup.js";
+
+// Для каждого попапа с формой
+export default class PopupWithForm extends Popup {
+  constructor({ handleSubmitForm }, popupSelector) {
+    super(popupSelector);
+    this._handleSubmitForm = handleSubmitForm; //колбек обработчик
+    this._inputs = this._popup.querySelectorAll(".popup__input");
+    this._form = this._popup.querySelector(".popup__form");
+ }
+  // Приватный метод собирает данные всех полей формы
+  _getInputValues(){
+    this._inputsValue = {};
+    this._inputs.forEach((input) => {
+      this._inputsValue[input.name] = input.value;
+    });
+    return this._inputsValue;
+  }
+  // Публичный перезаписанный метод
+  setEventListeners(){
+    //добавить обработчик сабмита формы
+    this._popup.addEventListener("submit", this._handleSubmitForm);
+    super.setEventListeners();
+  }
+  _removeEventListeners() {
+    this._popup.removeEventListener("submit", this._handleSubmitForm);
+    super._removeEventListeners();
+  }
+  // Публичный перезаписанный метод
+  close() {
+    //сбросить форму
+    this._form.reset();
+    super.close();
+  }
+}
