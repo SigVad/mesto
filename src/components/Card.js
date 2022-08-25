2a8fb16471b5dd903ca04f1ec72ee7fa085e6dab
SigVad <97845386+SigVad@users.noreply.github.com>
2022-08-25 11:27:19 +0300

    refact/убрать лишние переменные

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ export default class Card {
       // уникальные данные карты
       { link, name }, 
       // экземпляр и обработчик открытия картинки
-      { popupWithImage, handleCardClick }, 
+      { handleCardClick }, 
       // селектор шаблона карты
       templateSelector) {
     // селекторы элементов карты
@@ -18,49 +18,40 @@ export default class Card {
     // уникальные данные карты
     this._link = link;
     this._name = name;
-    // экземпляр и обработчик открытия картинки
-    this._popupWithImage = popupWithImage;
+    // обработчик открытия картинки
     this._handleCardClick = handleCardClick;
-    // селектор шаблона карты
-    this._templateSelector = templateSelector;
+    // шаблон карты
+    this._cardsTemplate =  document.querySelector(templateSelector);
   }
   // приватный метод делает разметку
   // забираем разметку из HTML и клонируем элемент
   _getTemplate() {
-    // найдёт template-элемент с классом card-template, извлечёт его содержимое,
-    // в содержимом найдёт элемент с классом card, клонирует его
-    const cardsTemplate = document.querySelector(this._templateSelector);
-    const cardElement = cardsTemplate.content
+    // в содержимом template-элемента найдёт элемент с классом card, клонирует его
+    this._element = this._cardsTemplate.content
                         .querySelector(this._cardClass).cloneNode(true);
-    // вернём DOM-элемент карточки
-    return cardElement;
   }
   //публичный метод добавления карты на страницу по шаблону (ссылка, название)
-  createCard() {//
-    // Запишем разметку в приватное поле _element. 
-    // Так у других элементов появится доступ к ней.
-    this._element = this._getTemplate();
-    // Добавим слушатели
-    this._setEventListeners();
-    this._cardImage = this._element.querySelector(this._imageClass)
+  createCard() {
+    this._getTemplate();
+    this._cardImage = this._element.querySelector(this._imageClass);
     // Добавим данные
     this._cardImage.src = this._link;
     this._cardImage.alt = this._name;
     this._element.querySelector(this._titleClass).textContent = this._name;
+    // Добавим слушатели
+    this._setEventListeners();
     // Вернём элемент наружу
     return this._element;
   }
   //метод добавления слушателей
   _setEventListeners() {
-    this._element.querySelector(this._likeButtonClass).addEventListener('click', this._handleLikeClick.bind(this));
+    this._likeButtonElement = this._element.querySelector(this._likeButtonClass);
+    this._likeButtonElement.addEventListener('click', this._handleLikeClick.bind(this));
     this._element.querySelector(this._trashButtonClass).addEventListener('click', this._handleTrashClick.bind(this));
-
-    this._element.querySelector(this._imageClass).addEventListener('click', this._handleCardClick.bind(this));
+    this._cardImage.addEventListener('click', this._handleCardClick.bind(this));
   }
   _handleLikeClick() {
-    this._element.querySelector(this._likeButtonClass)
-      .classList
-      .toggle(this._likeButtonActiveClass);
+    this._likeButtonElement.classList.toggle(this._likeButtonActiveClass);
   }
   _handleTrashClick() {
     //убрать разметку
